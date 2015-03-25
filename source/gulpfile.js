2a63d912bfa26fa687637260a4057909804e2347
revivaltech <revivaltek@gmail.com>
2015-03-25 17:57:54 +0100

    setup gulp add expressjs webserver and livereload

---
+++ source/gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')({port: 4002}));
+  app.use(express.static(__dirname));
+  app.listen(4000);
+});
+
+
+var tinylr;
+gulp.task('livereload', function() {
+  tinylr = require('tiny-lr')();
+  tinylr.listen(4002);
+});
+
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+
+gulp.task('default', ['express', 'livereload'], function() {
+
+});
